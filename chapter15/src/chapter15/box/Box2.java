c373fce7799272a2458ab0f65679cc6475b80709
소지니 <k012497@gmail.com>
2019-09-04 22:57:41 +0900

    generic simple ver.

---
+++ chapter15/src/chapter15/box/Box2.java
@@ -0,0 +1,10 @@
+package chapter15.box;
+
+public class Box2<Tv> {
+	private Tv data;
+	
+	public void print() {
+		System.out.println("hello");
+	}
+	
+}
---
+++ chapter15/src/chapter15/box/Test.java
@@ -0,0 +1,11 @@
+package chapter15.box;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Box2<Apple> aBox= new Box2<Apple>();
+		aBox.print();
+
+	}
+
+}
